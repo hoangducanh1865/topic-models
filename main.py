f4124f1bdc3010906490c5ddc5de0ebbaf1d58e6
hoangducanh1865 <hoangducanh.1865@gmail.com>
2025-07-27 18:20:05 +0700

    “Update”

---
+++ main.py
@@ -0,0 +1,25 @@
+from src.data.download_20ng import download_save
+from src.preprocess.preprocess import Preprocess
+from src.data.basic_dataset import BasicDataset
+from src.model.basic.ProdLDA import ProdLDA
+from src.trainer.basic.BasicTrainer import BasicTrainer
+from src.config.config import DEVICE
+
+def main():
+    data_dir = "./data/20ng_all"
+    download_save("./data")
+
+    preprocess = Preprocess(vocab_size=2000, verbose=True)
+    preprocess.preprocess_jsonlist(data_dir, label_name='group')
+
+    dataset = BasicDataset(dataset_dir=data_dir, batch_size=128, device=DEVICE, read_labels=True)
+
+    model = ProdLDA(vocab_size=dataset.vocab_size, num_topics=20, en_units=100, dropout=0.2).to(DEVICE)
+    trainer = BasicTrainer(model=model, dataset=dataset, epochs=10, batch_size=128, num_top_words=10, verbose=True)
+    top_words, train_theta = trainer.train()
+
+    for i, words in enumerate(top_words):
+        print(f"Topic {i}: {words}")
+
+if __name__ == "__main__":
+    main()
